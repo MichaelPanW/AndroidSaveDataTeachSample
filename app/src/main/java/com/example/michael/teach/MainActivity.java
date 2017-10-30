1786d0648bcfca1753eda6df4b9e4f8181fdc6f4
MichaelPenW <m28126584@gmail.com>
2017-10-30 22:25:45 +0800

    momo圖片顯示

--- app/src/main/java/com/example/michael/teach/MainActivity.java
+++ app/src/main/java/com/example/michael/teach/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.michael.teach;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageLoaderConfiguration configuration = ImageLoaderConfiguration.createDefault(MainActivity.this);
+        ImageLoader.getInstance().init(configuration);
+
+        String url="https://www.kpopn.com/wp-content/uploads/2016/08/20160825-momo.jpg";
+        ImageView img=(ImageView)findViewById(R.id.imageView);
+        ImageLoader imageLoader = ImageLoader.getInstance(); // Get singleton instance
+        imageLoader.displayImage(url, img);
+
     }
 }
